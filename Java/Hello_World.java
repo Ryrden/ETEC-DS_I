1a4cbceb1332da7508b2e79aeec9ae96344331ae
Ryan Souza <76923948+Ryrden@users.noreply.github.com>
2021-02-27 19:56:34 -0300

    Hello World em JAVA :D

---
+++ Java/Hello_World.java
@@ -0,0 +1,10 @@
+package Java;
+
+public class Hello_World {
+    public static void main(String[] arg) {
+        String MeuPrimeiroPrograma = "----------Hello World----------";
+
+        System.out.println(MeuPrimeiroPrograma);
+    }
+
+}
