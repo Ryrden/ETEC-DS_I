a1394554cba240f5a69123deed4467bf6fc94970
Ryan Souza <76923948+Ryrden@users.noreply.github.com>
2021-03-23 22:25:38 -0300

    Create Ex2_Sistema_eleitoral.java

---
+++ src/Java/Ex2_Sistema_eleitoral.java
@@ -0,0 +1,22 @@
+package Java;
+
+import java.util.Scanner;
+
+public class Ex2_Sistema_eleitoral {
+
+	public static void main(String[] args) {
+		Scanner input = new Scanner(System.in);
+		
+		System.out.print("Descubra se você está apto para votar, digite a sua idade: ");
+
+		int idade = input.nextInt();
+		
+		if (idade > 16) {
+			System.out.println("Você está apto para votar");
+		}else {
+			System.out.println("Você não está apto para votar");
+		}
+
+	}
+
+}
