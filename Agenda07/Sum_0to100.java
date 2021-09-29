b8c1e64fcddfeedffe87948c8be5916d936618d3
Ryan <ryansateles@gmail.com>
2021-09-29 02:53:50 -0300

    Estruturas de Repetição I

---
+++ Agenda07/Sum_0to100.java
@@ -0,0 +1,17 @@
+package Agenda07;
+
+public class Sum_0to100 {
+
+	public static void main(String[] args) {
+		
+		int soma = 0;
+		
+		for (int i = 0; i <= 100; i++) {
+			soma += i;
+		}
+		
+		System.out.println(soma);
+
+	}
+
+}
